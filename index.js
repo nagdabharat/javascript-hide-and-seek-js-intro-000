aeffe3ae2247dcceb89ebff49605fe1149fd771e
nagdabharat <66126759+nagdabharat@users.noreply.github.com>
2020-06-25 16:43:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function getFirstSelector(selector) {
+  document.querySelector(selector);
+}
+
+function nestedTarget() {
+  document.querySelector('#nested .target');
+}
+
+const list = document.querySelectorAll('ul .ranked-list');
+function increaseRankBy(n){
+Array.from(list).forEach((e) => {
+  parseInt(list[i].innerHTML) += parseInt(n);
+})
+}
 
